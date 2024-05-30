a2eba4a8fb23b3632293aa42e84e7e4aeb04e129
Cassy Cas <cassycas@CassydeMacBook-Air-3.local>
2024-05-29 21:56:26 -0700

    intial

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const PORT = process.env.PORT || 3000; // Set the port number
+const PORT = 3000;
 
-// Define routes and middleware here
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Define route handler for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public','index.html'));
+});
+
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
